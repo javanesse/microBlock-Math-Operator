f97171d3b6aa8c31e59d82511a76a558d02b8e6a
javanesse <javanesse@users.noreply.github.com>
2021-09-20 01:07:51 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
     name: "Missing Math Operator", // Category Name
     description: "Missing Math Value",
     author: "javanesse.net",
-    category: "Data Storage",
+    category: "Data Processing",
     version: "1.0.0",
     icon: "/static/icon.png", // Category icon
     color: "#F39C12", // Category color (recommend some blocks color)
